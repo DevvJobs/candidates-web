7a69616db7bfcd1a18591bf9741c213bddce728f
Ivan Kolmychek <ivan@devv.jobs>
2019-04-01 17:15:40 +0200

    Fix window is not defined error in pdf parsing (#54)

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -44,7 +44,8 @@ module.exports = {
     filename: '[name].js',
     publicPath: process.env.NODE_ENV === 'production'
       ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
+      : config.dev.assetsPublicPath,
+    globalObject: 'this'
   },
   resolve: {
     extensions: ['.js', '.vue', '.json', '.css'],
